d790d36a77e426a25ae7e4dca88d0f9fb52f7cc7
Nick <nduplessis11@gmail.com>
2024-08-23 13:45:46 -0400

    Add OpenAPI

--- src/Services/CurrentAccount/CurrentAccount.Api/Program.cs
+++ src/Services/CurrentAccount/CurrentAccount.Api/Program.cs
@@ -1,6 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "CurrentAccount service is running!");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapPost("/CurrentAccount/Initiate", () => "Stub!")
+    .WithName("CurrentAccountInitiate")
+    .WithOpenApi();
 
 app.Run();
