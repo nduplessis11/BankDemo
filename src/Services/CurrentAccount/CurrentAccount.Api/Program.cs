14fec9b32fdb5d26937ee373e0fb75d785aa2136
Nick <nduplessis11@gmail.com>
2024-08-25 07:00:46 -0400

    Connect to PSQL in Azure using system-assigned managed identity

--- src/Services/CurrentAccount/CurrentAccount.Api/Program.cs
+++ src/Services/CurrentAccount/CurrentAccount.Api/Program.cs
@@ -1,3 +1,7 @@
+using Azure.Core;
+using Azure.Identity;
+using Npgsql;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -18,5 +22,48 @@ app.UseHttpsRedirection();
 app.MapPost("/CurrentAccount/Initiate", () => "Stub!")
     .WithName("CurrentAccountInitiate")
     .WithOpenApi();
+
+
+
+// This example assumes you want to open the connection as part of an endpoint:
+app.MapGet("/test-connection", async () =>
+{
+    // Use system-assigned identity by default
+    var sqlServerTokenProvider = new DefaultAzureCredential();
+
+    // Uncomment this section and comment the line above if using user-assigned identity
+    /*
+    var sqlServerTokenProvider = new DefaultAzureCredential(
+        new DefaultAzureCredentialOptions
+        {
+            ManagedIdentityClientId = Environment.GetEnvironmentVariable("AZURE_POSTGRESQL_CLIENTID")
+        });
+    */
+
+    // Acquire the access token
+    AccessToken accessToken = await sqlServerTokenProvider.GetTokenAsync(
+        new TokenRequestContext(scopes: new string[]
+        {
+            "https://ossrdbms-aad.database.windows.net/.default"
+        }));
+
+    // Combine the token with the connection string from the environment variables provided by Service Connector
+    string connectionString =
+        $"{Environment.GetEnvironmentVariable("AZURE_POSTGRESQL_CONNECTIONSTRING")};Password={accessToken.Token}";
+
+    // Establish the connection
+    using (var connection = new NpgsqlConnection(connectionString))
+    {
+        Console.WriteLine("Opening connection using access token...");
+        await connection.OpenAsync();
+        
+        // Here you can perform database operations
+        // Example: return a success message
+        return Results.Ok("Database connection successful!");
+    }
+})
+.WithName("TestConnection")
+.WithOpenApi();
+
 
 app.Run();
