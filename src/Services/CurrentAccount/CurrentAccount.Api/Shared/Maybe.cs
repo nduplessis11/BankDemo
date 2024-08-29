7976651287d5b6dd4e8a228a989444222e3dac25
Nick <nduplessis11@gmail.com>
2024-08-29 08:53:12 -0400

    Add Maybe monad

---
+++ src/Services/CurrentAccount/CurrentAccount.Api/Shared/Maybe.cs
@@ -0,0 +1,32 @@
+﻿namespace CurrentAccount.Api.Shared;
+
+public class Maybe<T>
+{
+    public T? Value { get; private set; }
+
+    public bool HasValue { get; } = false;
+
+    private Maybe(T value)
+    {
+        Value = value;
+        HasValue = true;
+    }
+
+    private Maybe() => HasValue = false;
+
+    public static Maybe<T> Some(T value) => new(value);
+
+    public static Maybe<T> None() => new();
+
+    public Maybe<U> Map<U>(Func<T, U> transform)
+    {
+        if (HasValue) return Maybe<U>.Some(transform(Value!));
+        else return Maybe<U>.None();
+    }
+
+    public Maybe<U> Bind<U>(Func<T, Maybe<U>> transform)
+    {
+        if (HasValue) return transform(Value!);
+        else return Maybe<U>.None();
+    }
+}
