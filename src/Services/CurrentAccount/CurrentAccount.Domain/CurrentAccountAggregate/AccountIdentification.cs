cceff974a686a1b1dea3db6933d273f24703d0a2
Nick <nduplessis11@gmail.com>
2024-08-30 09:45:26 -0400

    Add CurrentAccountAggregate starting point

---
+++ src/Services/CurrentAccount/CurrentAccount.Domain/CurrentAccountAggregate/AccountIdentification.cs
@@ -0,0 +1,27 @@
+﻿namespace CurrentAccount.Domain.CurrentAccountAggregate;
+
+public sealed record AccountIdentification
+{
+    private AccountIdentification(string value, AccountIdentificationType accountIdentificationType)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException("Account number cannot be null or empty.", nameof(value));
+
+        IdentifierValue = value;
+        AccountIdentificationType = accountIdentificationType;
+    }
+
+    public AccountIdentificationType AccountIdentificationType { get; private set; }
+    public string IdentifierValue { get; }
+
+    public static AccountIdentification From(string value, AccountIdentificationType accountIdentificationType)
+    {
+        return new AccountIdentification(value, accountIdentificationType);
+    }
+}
+
+public enum AccountIdentificationType
+{
+    BBAN,
+    IBAN
+};
---
+++ src/Services/CurrentAccount/CurrentAccount.Domain/CurrentAccountAggregate/CurrentAccount.cs
@@ -0,0 +1,30 @@
+﻿namespace CurrentAccount.Domain.CurrentAccountAggregate;
+
+public readonly record struct CurrentAccountId(Guid Value)
+{
+    public static CurrentAccountId Empty { get; } = default;
+    public static CurrentAccountId Create() => new(Guid.NewGuid());
+    public override string ToString() => Value.ToString();
+}
+
+public sealed class CurrentAccount
+{
+    public CurrentAccountId Id { get; private set; }
+    public AccountIdentification CurrentAccountNumber { get; private set; }
+
+    // Parameterless constructor for EF
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor.
+    private CurrentAccount() { }
+#pragma warning restore CS8618
+
+    public static CurrentAccount Create(AccountIdentification currentAccountNumber)
+    {
+        CurrentAccount currentAccount = new()
+        {
+            Id = CurrentAccountId.Create(),
+            CurrentAccountNumber = currentAccountNumber
+        };
+
+        return currentAccount;
+    }
+}
